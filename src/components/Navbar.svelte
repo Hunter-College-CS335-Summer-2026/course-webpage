d0d69336f7cbc5ad11b1c9682c264eb6bc11b064
Sadab Hafiz <sadabhafizny@gmail.com>
2026-05-18 22:03:22 -0400

    dev: added prettier pre-commit hook

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 	let { title = 'Temp' } = $props();
 </script>
 
-<nav class="w-full bg-[#3F0157] px-2 py-5">
+<nav class="w-full bg-[#3F0157] p-5">
 	<a href={resolve('/')} class="font-sans text-4xl font-bold text-[#FCB827]">
 		{title}
 	</a>
