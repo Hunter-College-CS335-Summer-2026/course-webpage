44be438dd52d37015a5d2401f138293690e28624
Sadab Hafiz <sadabhafizny@gmail.com>
2026-05-28 02:43:08 -0400

    fix: use page instead of relying on layout only

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import Navbar from '../components/Navbar.svelte';
-	import favicon from '$lib/assets/favicon.ico';
-	import Schedule from '../components/Schedule.svelte';
-	import Overview from '../components/Overview.svelte';
-	import Footer from '../components/Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -13,10 +8,4 @@
 	<title>Software Design and Analysis III</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-<Navbar title="Software Design and Analysis III" />
-<div class="h-full w-full bg-linear-to-b from-[#FFFFFF] to-[#3f015730] p-2 pt-35 md:h-screen">
-	<Overview />
-	<Schedule />
-</div>
-<Footer />
 {@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Footer from '../components/Footer.svelte';
+	import Navbar from '../components/Navbar.svelte';
+	import Overview from '../components/Overview.svelte';
+	import Schedule from '../components/Schedule.svelte';
+</script>
+
+<Navbar title="Software Design and Analysis III" />
+<div class="h-full w-full bg-linear-to-b from-[#FFFFFF] to-[#3f015730] p-2 pt-35 md:h-screen">
+	<Overview />
+	<Schedule />
+</div>
+<Footer />
