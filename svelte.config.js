118b6eeb85d7f75ac1d8e86be307ed2a640bab9d
Sadab Hafiz <sadabhafizny@gmail.com>
2026-05-28 04:00:28 -0400

    fix: handle path issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: true
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html',
-			strict: 'true'
+			strict: false
 		}),
 		paths: {
-			base: process.argv.includes('build')
-				? '/Hunter-College-CS335-Summer-2026/course-webpage'
-				: '',
+			base: dev ? '' : '/course-webpage',
 			relative: false
 		},
 		prerender: {
