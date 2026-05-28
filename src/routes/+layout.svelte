2ced4f772495840653d703c4e1e7a4d697436879
Sadab Hafiz <sadabhafizny@gmail.com>
2026-05-28 14:22:16 -0400

    fix: properly load favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import './layout.css';
+	import { asset } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>Software Design and Analysis III</title>
-	<link rel="icon" href="/favicon.ico" />
+	<link rel="icon" href={asset('/favicon.ico')} />
 </svelte:head>
 {@render children()}
